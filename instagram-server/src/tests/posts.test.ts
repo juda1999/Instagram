b6d768773933de09e446e875ec007a6a17862452
Yehuda Segal <ysegal@TEN-D7G229NMQV.local>
2025-03-08 12:10:04 +0200

    fix up tests

--- instagram-server/src/tests/posts.test.ts
+++ instagram-server/src/tests/posts.test.ts
@@ -24,10 +24,9 @@ beforeAll(async () => {
   await postModel.deleteMany();
   await userModel.deleteMany();
 
-  await request(app).post("/auth/register").send(testUser);
-  const res = await request(app).post("/auth/login").send(testUser);
+  const res = await request(app).post("/auth/register").send(testUser);
   testUser.accessToken = res.body.accessToken;
-  testUser._id = res.body._id;
+  testUser._id = res.body.user._id;
   expect(testUser.accessToken).toBeDefined();
 });
 
@@ -39,31 +38,31 @@ afterAll((done) => {
 
 let postId = "";
 describe("Posts Tests", () => {
-  test("Posts test get all", async () => {
-    const response = await request(app).get("/post")
-          .set({ authorization: "JWT " + testUser.accessToken });
+  test("Posts test get first page", async () => {
+    const response = await request(app).post("/post").send({limit: 10, skip: 0})
+          .set({ authorization: testUser.accessToken });
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(0);
   });
 
   test("Test Create Post", async () => {
-    const response = await request(app).post("/post")
-      .set({ authorization: "JWT " + testUser.accessToken })
+    const response = await request(app).post("/post/create")
+      .set({ authorization: testUser.accessToken })
       .send({
         title: "Test title",
-        description: "Test description",
-        uploadedBy: testUser,
-        photo:"",
+        content: "Test description",
+        uploadedBy: testUser._id
       });
-    expect(response.statusCode).toBe(201);
+    expect(response.statusCode).toBe(200);
+    console.log(response.body)
     expect(response.body.title).toBe("Test title");
     expect(response.body.description).toBe("Test description");
     postId = response.body._id;
   });
 
-  test("Test get post by uploader", async () => {
-    const response = await request(app).get("/post?uploader=" + testUser._id)
-      .set({ authorization: "JWT " + testUser.accessToken });
+  test("Test get post by uploader first page", async () => {
+    const response = await request(app).post("/post/uploader?uploader=" + testUser._id).send({limit: 10, skip: 0})
+      .set({ authorization: testUser.accessToken });
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(1);
     expect(response.body[0].title).toBe("Test title");
@@ -72,45 +71,45 @@ describe("Posts Tests", () => {
 
   test("Test get post by id", async () => {
     const response = await request(app).get("/post/" + postId)
-      .set({ authorization: "JWT " + testUser.accessToken });
+      .set({ authorization: testUser.accessToken });
     expect(response.statusCode).toBe(200);
     expect(response.body.title).toBe("Test title");
     expect(response.body.description).toBe("Test description");
   });
 
   test("Test Create Post 2", async () => {
-    const response = await request(app).post("/post")
-      .set({ authorization: "JWT " + testUser.accessToken })
+    const response = await request(app).post("/post/create")
+      .set({ authorization: testUser.accessToken })
       .send({
         title: "Test Post 2",
         description: "Test Content 2",
-        uploadedBy: testUser,
+        uploadedBy: testUser._id,
       });
-    expect(response.statusCode).toBe(201);
+    expect(response.statusCode).toBe(200);
   });
 
   test("Posts test get all 2", async () => {
-    const response = await request(app).get("/post")
-      .set({ authorization: "JWT " + testUser.accessToken });
+    const response = await request(app).post("/post").send({ limit: 10, skip: 0 })
+      .set({ authorization: testUser.accessToken });
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(2);
   });
 
   test("Test Delete Post", async () => {
     const response = await request(app).delete("/post/" + postId)
-      .set({ authorization: "JWT " + testUser.accessToken });
+      .set({ authorization: testUser.accessToken });
     expect(response.statusCode).toBe(200);
     const response2 = await request(app).get("/post/" + postId)
-      .set({ authorization: "JWT " + testUser.accessToken });
+      .set({ authorization: testUser.accessToken });
     expect(response2.statusCode).toBe(404);
   });
 
   test("Test Create Post fail", async () => {
-    const response = await request(app).post("/post")
-      .set({ authorization: "JWT " + testUser.accessToken })
+    const response = await request(app).post("/post/create")
+      .set({ authorization: testUser.accessToken })
       .send({
         description: "Test description again",
       });
-    expect(response.statusCode).toBe(400);
+    expect(response.statusCode).toBe(500);
   });
 });

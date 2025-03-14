becf8b29dbbd344e6392cd1f2a035fe790f3bb47
mever-l <mor.lempert@gmail.com>
2025-03-14 21:46:52 +0200

    feat(deploy): add https and prod

--- instagram-server/src/app.ts
+++ instagram-server/src/app.ts
@@ -1,5 +1,20 @@
 import { initApp } from "./server";
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 
 const port = process.env.PORT;
 initApp().then(
-    (app) => app.listen(port, () => console.log(`App is listening on port: ${port}`)))
+    (app) =>  {
+        if(process.env.NODE_ENV !== 'Prod') {
+            console.log('This is development mode :)');
+        http.createServer(app).listen(port, () => console.log(`App is listening on port: ${port}`));
+        } else {
+            const options = {
+                key: fs.readFileSync('./client-key.pem'),
+                cert: fs.readFileSync('./client-cert.pem')
+            }
+            https.createServer(options, app).listen(port, () => console.log(`App is listening on port: ${port}`));
+            console.log('This is production mode :D');
+        }
+    })
